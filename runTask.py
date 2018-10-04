d1450e62aa97a8eaf7890cc3cec60bef3eed4b5a
Wang Chao <watcher.hangzhou@foxmail.com>
2018-10-04 16:40:42 +0800

    Create runTask.py

---
+++ runTask.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*
+
+
+from XunLeiDownloader import XunLeiDownloader;
+import urllib2
+import time
+import sys
+
+# 解决中文转码
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
+
+url = "http:/<host>:<port>/task.php?type=fetch_task"
+while True:
+	req = urllib2.Request(url)
+	if (req):
+		try:
+			res_data = urllib2.urlopen(req)
+			task = res_data.read()
+			if (task):
+				print task
+				xld = XunLeiDownloader()
+				xld.startDownload(task)
+				xld = None
+		
+		finally:
+			pass
+	time.sleep(5)
+
+
+
+
